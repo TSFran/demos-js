83b90db2a6c070acc6e3f555cc579203bf965680
TSFran <luistupa6@gmail.com>
2018-06-17 18:20:47 -0500

    feat(site):Se implementa maquetacion

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,42 @@ var pug = require('gulp-pug');
 const babel = require('gulp-babel');
 
 var path = {
-    stylus : ['stylus/*.styl'],
-    css : 'public/static/css/'
+  stylus: ['stylus/*.styl'],
+  css: 'public/static/css/'
 };
 
-gulp.task('css', function () {
-    return gulp.src('frontend/src/stylus/*.styl')
-      .pipe(stylus({
-          compress : false
-      }))
-      .pipe(gulp.dest('public/static/css/'));
-  });
+gulp.task('css', function() {
+  return gulp
+    .src('frontend/src/stylus/*.styl')
+    .pipe(
+      stylus({
+        compress: false
+      })
+    )
+    .pipe(gulp.dest('public/static/css/'));
+});
 
-gulp.task('html', function (){
-    return gulp.src('frontend/src/pug/*.pug')
-    .pipe(pug({pretty: true}))
+gulp.task('html', function() {
+  return gulp
+    .src('frontend/src/pug/*.pug')
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest('public/static/html/'));
-})
+});
 
+gulp.task('js', function() {
+  return gulp
+    .src('frontend/src/js/*.js')
+    .pipe(
+      babel({
+        presets: ['env']
+      })
+    )
+    .pipe(gulp.dest('public/static/js/dist/'));
+});
 
-gulp.task('js', function () {
-    return gulp.src('frontend/src/js/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('public/static/js/dist/'));
-})
-
-gulp.task('watch', function (){
-    gulp.watch('frontend/src/stylus/*.styl', ['css']);
-    gulp.watch('frontend/src/pug/*.pug', ['html']);
-    gulp.watch('frontend/src/js/*.js', ['js']);
-})
-
-
+gulp.task('watch', function() {
+  gulp.watch('frontend/src/stylus/*.styl', ['css']);
+  gulp.watch('frontend/src/pug/*.pug', ['html']);
+  gulp.watch('frontend/src/pug/Components/*.pug', ['html']);
+  gulp.watch('frontend/src/js/*.js', ['js']);
+});
